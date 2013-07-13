7fffc623aa1d1f0769237135866200b457fe560e
Francesco Lippolis <devilsdeth@hotmail.it>
2013-07-13 18:10:34 +0200

    beta

--- Piperka_Opener.user.js
+++ Piperka_Opener.user.js
@@ -6,9 +6,31 @@
 // @version     1
 // @grant       none
 
-
-for(i=0;i<100;i++){
-	document.getElementsByTagName("li")[i].id=i;
-	alert(i);
+	function findUpdates(el){
+		var patt1 = new RegExp("\\((\\d+) new\\)");
+		return patt1.exec(el.innerHTML)[1];
 	}
+
+	var list = document.getElementsByTagName("li");
+	var i = list.length-1
+	var up = findUpdates(list[i]);
+	alert("\\((\\d+) new\\)")
+	if(up>20){
+		alert("No minor updates available.");
+		return 0}
+	var flag=0;
+	if(up>=3){
+		alert("The opened comics will have three or more updates.");
+		flag=1;
+	}
+	if(up<3) alert("The opened comics will have "&n&" update");
+	var n=up;
+	while(i>=0&&n<21&&(flag||n==up)){
+		window.open(list[i].firstChild.href);
+		i--;
+		n=findUpdates(list[i]);
+	}
+		
+	
+	
 // ==/UserScript==
